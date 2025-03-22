ec60de177e6386a27d2a3ea7208858562dad9a51
Alefer Reinert <aleferreinert@gmail.com>
2025-03-22 16:11:59 -0300

    refactor(Logo): accessibility and tests

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -18,13 +18,18 @@ export default meta
 type Story = StoryObj<typeof Logo>
 
 export const Default: Story = {
-  play: ({ canvasElement }) => {
+  play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement)
-    const logo = canvas.getByLabelText(/won games/i)
-    const wrapper = canvas.getByTestId('LogoComponent')
 
-    expect(logo).toHaveStyle({ color: theme.colors.white })
-    expect(wrapper).toHaveStyle({ width: remToPx('11rem') })
+    await step('Color white', () => {
+      const logo = canvas.getByRole('img', { name: 'Won Games' })
+      expect(logo).toHaveStyle({ color: theme.colors.white })
+    })
+
+    await step('Size medium', () => {
+      const wrapper = canvas.getByTestId('LogoComponent')
+      expect(wrapper).toHaveStyle({ width: remToPx('11rem') })
+    })
   }
 }
 
@@ -37,11 +42,13 @@ export const Black: Story = {
       default: 'Light'
     }
   },
-  play: ({ canvasElement }) => {
+  play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement)
-    const logo = canvas.getByLabelText(/won games/i)
 
-    expect(logo).toHaveStyle({ color: theme.colors.black })
+    await step('Color black', () => {
+      const logo = canvas.getByRole('img', { name: 'Won Games' })
+      expect(logo).toHaveStyle({ color: theme.colors.black })
+    })
   }
 }
 
@@ -49,11 +56,13 @@ export const Small: Story = {
   args: {
     size: 'small'
   },
-  play: ({ canvasElement }) => {
+  play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement)
-    const wrapper = canvas.getByTestId('LogoComponent')
 
-    expect(wrapper).toHaveStyle({ width: remToPx('5.8rem') })
+    await step('Size small', () => {
+      const wrapper = canvas.getByTestId('LogoComponent')
+      expect(wrapper).toHaveStyle({ width: remToPx('5.8rem') })
+    })
   }
 }
 
@@ -61,26 +70,33 @@ export const Large: Story = {
   args: {
     size: 'large'
   },
-  play: ({ canvasElement }) => {
+  play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement)
-    const wrapper = canvas.getByTestId('LogoComponent')
 
-    expect(wrapper).toHaveStyle({
-      width: remToPx('20rem')
+    await step('Size large', () => {
+      const wrapper = canvas.getByTestId('LogoComponent')
+      expect(wrapper).toHaveStyle({
+        width: remToPx('20rem')
+      })
     })
   }
 }
 
 export const WithoutText: Story = {
   args: {
-    $hideText: true
+    $withoutText: true
   },
-  play: ({ canvasElement }) => {
+  play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement)
-    const wrapper = canvas.getByTestId('LogoComponent')
-    const text = canvas.getByLabelText('won')
 
-    expect(wrapper).toBeVisible()
-    expect(text).not.toBeVisible()
+    await step('Logo visible', () => {
+      const wrapper = canvas.getByTestId('LogoComponent')
+      expect(wrapper).toBeVisible()
+    })
+
+    await step('Text not visible', () => {
+      const text = canvas.getByTestId('LogoText')
+      expect(text).not.toBeVisible()
+    })
   }
 }
