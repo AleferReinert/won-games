62e0a31d2f745dc9ef576d6bf03187730b2a7fd8
Alefer Reinert <aleferreinert@gmail.com>
2025-04-21 10:26:23 -0300

    feat: create success page

---
+++ src/pages/success/index.tsx
@@ -0,0 +1,67 @@
+import Heading from 'components/Heading/Heading'
+import Showcase, { ShowcaseProps } from 'components/Showcase/Showcase'
+import { RECOMMENDED_PRODUCTS } from 'graphql/queries/recommendedProducts'
+import { useCart } from 'hooks/useCart'
+import Link from 'next/link'
+import { GetServerSidePropsContext } from 'next/types'
+import { useEffect, type ReactElement } from 'react'
+import DefaultTemplate from 'templates/Default/Default'
+import { Query } from 'types/generated'
+import { initializeApollo } from 'utils/apollo'
+import { highlightMapper, productMapper } from 'utils/mappers'
+import { requireAuth } from 'utils/requireAuth'
+import * as S from './success.styles'
+
+interface SuccessPageProps {
+  recommendedSection: ShowcaseProps
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { session } = await requireAuth(context)
+  const apolloClient = initializeApollo({ session })
+  const { data } = await apolloClient.query<Pick<Query, 'recommended'>>({
+    query: RECOMMENDED_PRODUCTS
+  })
+  const { title, highlight, products } = data.recommended.data.attributes
+
+  return {
+    props: {
+      recommendedSection: {
+        title,
+        highlight: highlightMapper(highlight),
+        products: productMapper(products)
+      }
+    }
+  }
+}
+
+const SuccessPage = ({ recommendedSection }: SuccessPageProps) => {
+  const { clearCart } = useCart()
+
+  useEffect(() => clearCart(), [clearCart])
+
+  return (
+    <>
+      <S.Wrapper>
+        <Heading as='h1'>Your purchase was successful!</Heading>
+        <S.Svg alt='' src='/img/success-checked.svg' width={100} height={100} aria-hidden />
+        <S.Description>
+          Save your payment details by email. <br />
+          Your game is now available for download <Link href='/account/orders'>here</Link>.
+        </S.Description>
+      </S.Wrapper>
+
+      <Showcase
+        title={recommendedSection.title}
+        highlight={recommendedSection.highlight}
+        products={recommendedSection.products}
+      />
+    </>
+  )
+}
+
+SuccessPage.getLayout = function getLayout(page: ReactElement) {
+  return <DefaultTemplate>{page}</DefaultTemplate>
+}
+
+export default SuccessPage
