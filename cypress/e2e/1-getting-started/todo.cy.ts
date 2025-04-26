f0bc289170950dae44b78f6e07048847c94f4249
Alefer Reinert <aleferreinert@gmail.com>
2025-04-26 09:39:05 -0300

    test: install and configure Cypress

---
+++ cypress/e2e/1-getting-started/todo.cy.ts
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+  it('visits the google page', () => {
+    cy.google()
+  })
+})
---
+++ cypress/support/index.d.ts
@@ -0,0 +1,8 @@
+declare namespace Cypress {
+  interface Chainable {
+    /** Custom command to visit google
+     * @example cy.google()
+     */
+    google(): Chainable<AUTWindow>
+  }
+}
