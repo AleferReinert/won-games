8ffddbba05a76a24234340e510744e7fcff4a8e8
Alefer Reinert <aleferreinert@gmail.com>
2025-04-27 20:45:30 -0300

    test(home): add e2e tests for banner and sections

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,9 +4,24 @@ describe('Home page', () => {
   it('Banner, New Releases, Most Populars, Coming Soon and Free Games', () => {
     cy.visit('/')
     cy.toggleBanner()
-    cy.get(`[data-cy='newReleases']`).should('be.visible')
-    cy.get(`[data-cy='mostPopulars']`).should('be.visible')
-    cy.get(`[data-cy='comingSoon']`).should('be.visible')
-    cy.get(`[data-cy='freeGames']`).should('be.visible')
+    cy.get('[data-cy="newReleases"]').within(() => {
+      cy.findByRole('heading', { level: 2 }).should('be.visible')
+      cy.findByTestId('ProductSliderComponent').should('be.visible')
+    })
+    cy.get('[data-cy="mostPopulars"]').within(() => {
+      cy.findByRole('heading', { level: 2 }).should('be.visible')
+      cy.findByTestId('HighlightComponent').should('be.visible')
+      cy.findByTestId('ProductSliderComponent').should('be.visible')
+    })
+    cy.get('[data-cy="comingSoon"]').within(() => {
+      cy.findByRole('heading', { level: 2 }).should('be.visible')
+      cy.findByTestId('HighlightComponent').should('be.visible')
+      cy.findByTestId('ProductSliderComponent').should('be.visible')
+    })
+    cy.get('[data-cy="freeGames"]').within(() => {
+      cy.findByRole('heading', { level: 2 }).should('be.visible')
+      cy.findByTestId('HighlightComponent').should('be.visible')
+      cy.findByTestId('ProductSliderComponent').should('be.visible')
+    })
   })
 })
--- cypress/support/cypress.d.ts
+++ cypress/support/cypress.d.ts
@@ -1,4 +1,3 @@
-// cypress\support\index.d.ts
 declare namespace Cypress {
   interface Chainable {
     /** Switch between BannerSlider slides and verify banner visibility */
