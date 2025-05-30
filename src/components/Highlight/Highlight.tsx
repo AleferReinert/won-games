87ac04b1a30eca15a48ac4df039518854561c655
Alefer Reinert <aleferreinert@gmail.com>
2025-05-30 20:31:55 -0300

    style(Highlight): correct position of elements on mobile

--- src/components/Highlight/Highlight.tsx
+++ src/components/Highlight/Highlight.tsx
@@ -31,7 +31,9 @@ export const Highlight = ({
     <div className='lg:mx-auto lg:px-4 max-w-container'>
       <div
         data-testid='HighlightComponent'
-        className={`relative flex h-[230px] justify-between mb-8 md:h-[320px] ${alignment === 'right' ? 'flex-row' : 'flex-row-reverse'}`}
+        className={`relative flex h-[230px] justify-between mb-8 lg:h-[320px] 
+					${alignment === 'right' ? 'flex-row' : 'flex-row-reverse'} 
+					`}
       >
         <Image
           src={background.url}
@@ -47,15 +49,18 @@ export const Highlight = ({
               fill
               aria-hidden={floatImg.alternativeText ? false : true}
               sizes='(max-width: 768px) 100vw, 50vw'
-              className='min-w-[100px] max-h-[230px] max-w-full h-auto! static! md:max-h-[320px]'
+              className='min-w-[100px] max-h-[230px] max-w-full h-auto! static! pt-8 mr-4 md:max-h-[320px]'
             />
           )}
         </div>
         <div
-          className={`z-10 p-4 text-zinc-50 self-start md:p-10 md:self-end ${alignment === 'right' ? 'text-right pl-0' : 'pr-0'}`}
+          className={`z-10 px-4 py-8 text-zinc-50 md:p-12 lg:self-end
+						${alignment === 'right' ? 'text-right pl-0' : 'pr-0'} 
+						${floatImg ? 'self-start' : 'self-end'} 
+						`}
         >
-          <h3 className='text-lg font-semibold md:text-[28px]'>{title}</h3>
-          <p className='text-sm font-light mb-4 md:mb-8 md:text-lg'>{description}</p>
+          <h3 className='text-lg font-semibold lg:text-[28px]'>{title}</h3>
+          <p className='text-sm font-light mb-4 lg:mb-8 lg:text-lg'>{description}</p>
           <Button asLink href={buttonUrl}>
             {buttonLabel}
           </Button>
