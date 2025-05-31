6467afdfc07727cfd39c584d21ec30fb210be546
Alefer Reinert <aleferreinert@gmail.com>
2025-05-30 21:34:18 -0300

    feat(Gallery): close modal on overlay click, add hover on arrows and close

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -87,17 +87,21 @@ export const Gallery = ({ items }: GalleryProps) => {
       <div
         aria-label='modal'
         aria-hidden={!modal}
+        onClick={() => setModal(false)}
         className={`${styles.modal} fixed inset-0 bg-black/85 transition-opacity ease-in-out duration-300 flex justify-center items-center z-40 p-6`}
       >
         <button
           aria-label='Close modal'
           title='Close'
-          onClick={() => setModal(false)}
-          className='absolute top-0 right-0 p-2 text-right text-zinc-50 z-30 cursor-pointer'
+          onClick={(e) => {
+            e.stopPropagation()
+            setModal(false)
+          }}
+          className='absolute top-0 right-0 p-2 text-right text-zinc-50 z-30 cursor-pointer transition hover:scale-90'
         >
           <MdOutlineClose aria-hidden role='img' className='size-6 fill-zinc-50' />
         </button>
-        <div className='aspect-video max-w-4/5 max-h-full 3xl:max-w-container'>
+        <div onClick={(e) => e.stopPropagation()} className='aspect-video max-w-4/5 max-h-full 3xl:max-w-container'>
           <Slider ref={slider} settings={modalSettings}>
             {items.map((item, index) => (
               <Image
