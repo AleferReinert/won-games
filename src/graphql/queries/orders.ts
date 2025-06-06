d6c86b04495d092c475b160051338a55d0ec95cb
Alefer Reinert <aleferreinert@gmail.com>
2025-06-06 19:53:51 -0300

    feat(queries/orders): sort by recents

--- src/graphql/queries/orders.ts
+++ src/graphql/queries/orders.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client'
 
 export const ORDERS = gql`
   query Orders($userEmail: String) {
-    orders(filters: { users_permissions_user: { email: { eq: $userEmail } } }) {
+    orders(filters: { users_permissions_user: { email: { eq: $userEmail } } }, sort: "createdAt:desc") {
       documentId
       card_brand
       card_last4
