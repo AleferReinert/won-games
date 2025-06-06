25f800c14da62fface35b21be29edcad7031dca1
Alefer Reinert <aleferreinert@gmail.com>
2025-06-06 20:45:17 -0300

    feat(storybook/main): ignore unused tests to chromatic

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,16 @@ process.env.NEXT_PUBLIC_IMAGE_HOST = ''
 dotenv.config()
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '!../src/app/(main)/(home)/page.stories.tsx',
+    '!../src\app\(main)\account\layout.stories.tsx',
+    '!../src\app\(main)\account\orders\OrdersPage.stories.tsx',
+    '!../src\app\(main)\account\profile\ProfilePage.stories.tsx',
+    '!../src\app\(main)\explore\ExplorePage.stories.tsx',
+    '!../src\app\(main)\game\[slug]\ProductPage.stories.tsx',
+    '!../src\app\(main)\wishlist\WishlistPage.stories.tsx'
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
