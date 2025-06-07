1865cf20bece13d47e3899762c62492e1bd9b5f2
Alefer Reinert <aleferreinert@gmail.com>
2025-06-07 15:35:58 -0300

    style(Banner): change bg of caption and set full on mobile, change space and cursor on dots

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -48,24 +48,26 @@ export const Banner = ({
         />
       </div>
       {caption && (
-        <div className='w-full p-6 bg-[black]/35 md:bg-[black]/70 md:p-10 absolute bottom-0 left-0'>
-          {title && <h2 className='text-lg font-semibold text-zinc-50 md:text-[28px]'>{title}</h2>}
-          {description && (
-            <p
-              dangerouslySetInnerHTML={{ __html: description }}
-              className='text-zinc-50 text-sm font-normal [&_strong]:text-theme-primary [&_strong]:font-semibold md:text-lg'
-            />
-          )}
-          {buttonUrl && (
-            <>
-              <Button asLink href={buttonUrl} className='mt-4 md:hidden!'>
-                {buttonLabel}
-              </Button>
-              <Button asLink href={buttonUrl} size='large' className='mt-4 hidden! md:inline-flex!'>
-                {buttonLabel}
-              </Button>
-            </>
-          )}
+        <div className='w-full p-6 flex flex-col justify-end bg-[black]/50 sm:bg-transparent sm:from-black to-transparent bg-linear-to-t md:p-10 absolute bottom-0 left-0 top-0 sm:top-1/3'>
+          <div>
+            {title && <h2 className='text-lg font-semibold text-zinc-50 md:text-[28px]'>{title}</h2>}
+            {description && (
+              <p
+                dangerouslySetInnerHTML={{ __html: description }}
+                className='text-zinc-50 text-sm font-normal [&_strong]:text-theme-primary [&_strong]:font-semibold md:text-lg'
+              />
+            )}
+            {buttonUrl && (
+              <>
+                <Button asLink href={buttonUrl} className='mt-4 md:hidden!'>
+                  {buttonLabel}
+                </Button>
+                <Button asLink href={buttonUrl} size='large' className='mt-4 hidden! md:inline-flex!'>
+                  {buttonLabel}
+                </Button>
+              </>
+            )}
+          </div>
         </div>
       )}
     </div>
